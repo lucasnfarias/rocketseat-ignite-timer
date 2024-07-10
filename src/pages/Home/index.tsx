c319c629114ae0e7dd92db959ede143dc0212b96
Lucas Farias <lucas.farias@rtbhouse.com>
2024-07-09 21:03:52 -0300

    create Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,60 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskNameInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskNameInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Pera"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
