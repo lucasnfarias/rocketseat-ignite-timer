71354c79ee91001f99c59f3b9a6f0e7f09138987
Lucas Farias <lucas.farias@rtbhouse.com>
2024-07-13 15:06:40 -0300

    use CyclesContext on history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { useContext } from "react";
 import { CyclesContext } from "../../contexts/cycles";
 import { HistoryContainer, HistoryList, Status } from "./styles";
@@ -8,8 +10,6 @@ export function History() {
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
-
-      <pre>{JSON.stringify(cycles, null, 2)}</pre>
 
       <HistoryList>
         <table>
@@ -23,89 +23,53 @@ export function History() {
           </thead>
 
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="yellow">Em andamento</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="red">Interrompido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
+            {cycles.map(
+              ({
+                id,
+                task,
+                minutesAmount,
+                startDate,
+                finishedDate,
+                interruptedDate,
+                status,
+              }) => {
+                const publishedDateRelativeToNow = formatDistanceToNow(
+                  startDate,
+                  {
+                    locale: ptBR,
+                    addSuffix: true,
+                  }
+                );
+                return (
+                  <tr key={id}>
+                    <td>{task}</td>
+                    <td>{minutesAmount} minutos</td>
+                    <td>{publishedDateRelativeToNow}</td>
+                    <td>
+                      {status === "ongoing" && (
+                        <Status statusColor="yellow">Em andamento</Status>
+                      )}
+                      {status === "interrupted" && (
+                        <Status
+                          title={interruptedDate?.toDateString()}
+                          statusColor="red"
+                        >
+                          Interrompido
+                        </Status>
+                      )}
+                      {status === "finished" && (
+                        <Status
+                          title={finishedDate?.toDateString()}
+                          statusColor="green"
+                        >
+                          Concluído
+                        </Status>
+                      )}
+                    </td>
+                  </tr>
+                );
+              }
+            )}
           </tbody>
         </table>
       </HistoryList>
